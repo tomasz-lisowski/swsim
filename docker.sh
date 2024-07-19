8b9a77ae3ac1abf0f8019b3d87cdffe00fb3b895
Tomasz Lisowski <t.p.lisowski@gmail.com>
2024-07-18 21:31:09 -0300

    Ensure docker-built files are owned by the user instead of root

--- docker.sh
+++ docker.sh
@@ -4,4 +4,4 @@ set -o pipefail; # Don't hide errors within pipes.
 set -o errexit;  # Abort on non-zero exit status.
 
 docker build --progress=plain . -t tomasz-lisowski/swsim:1.0.0 2>&1 | tee docker.log;
-docker run -v .:/opt/swsim --tty --interactive --rm tomasz-lisowski/swsim:1.0.0;
+docker run --user $(id -u):$(id -g) -v .:/opt/swsim --tty --interactive --rm tomasz-lisowski/swsim:1.0.0;
